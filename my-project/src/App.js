825285125403c2c3bc2a178a0229f309aa8e83b0
anil843324 <anilkrsingh14200@gmail.com>
2022-07-31 15:07:54 +0530

    about component done

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
 
       <Navbar/>
       <Hero/>
+      <About/>
 
     </div>
   );
